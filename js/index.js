abba0fe27dd8c5b8c3b74d00638c6f1a77411c57
dongxiuling <644961699@qq.com>
2020-06-30 13:35:02 +0800

    样式fix

--- js/index.js
+++ js/index.js
@@ -3,46 +3,46 @@
 // $('.roll-content').width($width * $num);
 
 
-var oul = $('.roll-content');
-var oulHtml = oul.html();
-oul.html(oulHtml + oulHtml)
-var timeId = null;
+// var oul = $('.roll-content');
+// var oulHtml = oul.html();
+// oul.html(oulHtml + oulHtml)
+// var timeId = null;
 
-var ali = $('.roll-content li');
-var aliWidth = ali.eq(0).outerWidth(true);
-var aliSize = ali.size();
-var ulWidth = aliWidth * aliSize;
-oul.width(ulWidth);	//1600px
+// var ali = $('.roll-content li');
+// var aliWidth = ali.eq(0).outerWidth(true);
+// var aliSize = ali.size();
+// var ulWidth = aliWidth * aliSize;
+// oul.width(ulWidth);	//1600px
 
-var speed = -2;
+// var speed = -2;
 
-function slider() {
+// function slider() {
 
-    if (speed < 0) {
-        if (oul.css('left') == -ulWidth / 2 + 'px') {
-            oul.css('left', 0);
-        }
-        oul.css('left', '+=-2px');
-    }
+//     if (speed < 0) {
+//         if (oul.css('left') == -ulWidth / 2 + 'px') {
+//             oul.css('left', 0);
+//         }
+//         oul.css('left', '+=-2px');
+//     }
 
 
-    if (speed > 0) {
-        if (oul.css('left') == '0px') {
-            oul.css('left', -ulWidth / 2 + 'px');
-        }
-        oul.css('left', '+=' + speed + 'px');
-    }
+//     if (speed > 0) {
+//         if (oul.css('left') == '0px') {
+//             oul.css('left', -ulWidth / 2 + 'px');
+//         }
+//         oul.css('left', '+=' + speed + 'px');
+//     }
 
-}
+// }
 
-// setInterval()函数的作用是：每隔一段时间，执行该函数里的代码
-timeId = setInterval(slider, 30);
+// // setInterval()函数的作用是：每隔一段时间，执行该函数里的代码
+// timeId = setInterval(slider, 30);
 
-$('.roll-content').mouseover(function () {
-    // clearInterval()函数的作用是用来清除定时器
-    clearInterval(timeId);
-});
+// $('.roll-content').mouseover(function () {
+//     // clearInterval()函数的作用是用来清除定时器
+//     clearInterval(timeId);
+// });
 
-$('.roll-content').mouseout(function () {
-    timeId = setInterval(slider, 30);
-});
+// $('.roll-content').mouseout(function () {
+//     timeId = setInterval(slider, 30);
+// });
